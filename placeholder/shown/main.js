f885d1b80f5609f17bae6f79f54a11372fd0e94a
Eduard Teixeira <eduard.teixeira01@gmail.com>
2021-12-17 11:16:30 -0300

    placeholder

---
+++ placeholder/shown/main.js
@@ -0,0 +1,11 @@
+const input = document.querySelector('input');
+
+input.addEventListener('focus', () => {
+    //console.log('focus');
+    // Add parent class to move label above input
+});
+
+input.addEventListener('blur', () => {
+    //console.log('blur');
+    // Remove parent class to reset label
+});
