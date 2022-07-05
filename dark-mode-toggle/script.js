511e25874ec4a71eb21a7bdd4e972dc64db467a6
EduardTeixeira <eduardteixeira01@gmail.com>
2022-07-05 17:38:05 -0300

    dark mode toggle animation using css

---
+++ dark-mode-toggle/script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.querySelector(".container").addEventListener("click", () => {
+        document.querySelector(".sun-logo").classList.toggle("animate-sun");
+        document.querySelector(".moon-logo").classList.toggle("animate-moon");
+        document.querySelector("body").classList.toggle("dark");
+    })
+}
